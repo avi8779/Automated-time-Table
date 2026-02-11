13000daec112e1f39053233c88b1f2aba74a6731
Avinash kumar <avi87793919@gmail.com>
2026-02-11 11:08:29 +0530

    setup of frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind CSS is Working 🚀
+      </h1>
+    </div>
+  );
+}
