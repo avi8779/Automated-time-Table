6dd72d3c619976052c58907c19de1b6038dc29bf
Avinash kumar <123002279+avi8779@users.noreply.github.com>
2026-06-30 21:50:26 +0530

    Refactor server.js by cleaning comments and logs
    Removed comments and updated server log message.

--- Server/server.js
+++ Server/server.js
@@ -7,17 +7,14 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-    // 🔹 DB connection test
+    // DB connection test
     const [ping] = await pool.query("SELECT 1 AS db_status");
     console.log("✅ DB Connected:", ping);
 
-    // 🔹 REAL DATA FETCH (example: departments)
-    // const [departments] = await pool.query("SELECT * FROM department");
-    // console.log("📦 Departments Data:");
-    // console.table(departments);   // 👈 BEST for terminal
+    
 
     app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
+        console.log(`🚀 Server running on port ${PORT}`);
     });
 
   } catch (error) {
